f9260d4b1e8dd613f4c5053ec006c375ec7dd5ad
Søren Nguyen Boisen <snb@hivepeople.dk>
2015-09-01 15:21:29 +0200

    Added console project to try to build a program that exhibits the bug

---
+++ RyuJIT.Bug.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using RyuJIT.Bug.Helper;
+
+namespace RyuJIT.Bug.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Awaiting task...");
+
+            var completedTask = Task.FromResult(1);
+            var helper = new RyuJitBugHelper(completedTask);
+            helper.AwaitTaskCompletion().Wait();
+
+            Console.WriteLine("done");
+        }
+    }
+}
